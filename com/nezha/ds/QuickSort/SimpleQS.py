11ff4d3e8274bc99fa4727ba9318d9e564103f7c
nezha <nezhaxiaozi@outlook.com>
2017-03-12 14:48:14 +0800

    Initial commit

---
+++ com/nezha/ds/QuickSort/SimpleQS.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+'''
+    @Des: 快速排序超级简化版本
+'''
+def qsort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        return qsort([x for x in arr[1:] if x < pivot]) + \
+               [pivot] + \
+               qsort([x for x in arr[1:] if x >= pivot])
